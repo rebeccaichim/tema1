8c28b7be36f7f6e4d8a3b93f2551ec4b8844c7f0
Rebecca <rebeccaichim@gmail.com>
2023-10-20 11:09:25 +0300

    commit check

--- src/TesteProblem4.java
+++ src/TesteProblem4.java
@@ -1,2 +1,73 @@
+import java.util.Arrays;
 public class TesteProblem4 {
+    public void testFindeBilligsteTastaturCorect() {
+        int[] tastaturen = {40, 35, 70, 15, 45};
+        int rezultatAsteptat = 15;
+        int rezultatObtinut = Problem4.findeBilligsteTastatur(tastaturen);
+        assert rezultatAsteptat == rezultatObtinut;
+    }
+    public void testFindeBilligsteTastaturFalsCorect() {
+        int[] tastaturen = {};
+        int rezultatAsteptat = -1;
+        int rezultatObtinut = Problem4.findeBilligsteTastatur(tastaturen);
+        assert rezultatAsteptat == rezultatObtinut;
+    }
+    public void testFindeTeuerstenGegenstandCorect() {
+        int[] tastaturen = {15, 20, 10, 35};
+        int[] usbLaufwerke = {20, 15, 40, 15};
+        int rezultatAsteptat = 40;
+        int rezultatObtinut = Problem4.findeTeuerstenGegenstand(tastaturen, usbLaufwerke);
+        assert rezultatAsteptat == rezultatObtinut;
+    }
+    public void testFindeTeuerstenGegenstandFalsCorect() {
+        int[] tastaturen = {};
+        int[] usbLaufwerke = {};
+        int rezultatAsteptat = 0;
+        int rezultatObtinut = Problem4.findeTeuerstenGegenstand(tastaturen, usbLaufwerke);
+        assert rezultatAsteptat == rezultatObtinut;
+    }
+    public void testFindeTeuerstesUsbLaufwerkCorect() {
+        int[] usbLaufwerke = {15, 45, 20};
+        int budget = 30;
+        int rezultatAsteptat = 20;
+        int rezultatObtinut = Problem4.findeTeuerstesUsbLaufwerk(usbLaufwerke, budget);
+        assert rezultatAsteptat == rezultatObtinut;
+    }
+    public void testFindeTeuerstesUsbLaufwerkFalsCorect() {
+        int[] usbLaufwerke = {25, 35};
+        int budget = 20;
+        int rezultatAsteptat = -1;
+        int rezultatObtinut = Problem4.findeTeuerstesUsbLaufwerk(usbLaufwerke, budget);
+        assert rezultatAsteptat == rezultatObtinut;
+    }
+
+    public void testBerechneAusgabenCorect() {
+        int[] tastaturen = {40, 50, 60};
+        int[] usbLaufwerke = {8, 12};
+        int budget = 60;
+        int rezultatAsteptat = 50 + 8;
+        int rezultatObtinut = Problem4.berechneAusgaben(budget, tastaturen, usbLaufwerke);
+        assert rezultatAsteptat == rezultatObtinut;
+    }
+    public void testBerechneAusgabenFalsCorect() {
+        int[] tastaturen = {40, 70};
+        int[] usbLaufwerke = {50};
+        int budget = 60;
+        int rezultatAsteptat = -1;
+        int rezultatObtinut = Problem4.berechneAusgaben(budget, tastaturen, usbLaufwerke);
+        assert rezultatAsteptat == rezultatObtinut;
+    }
+
+    public static void main(String[] args) {
+
+        TesteProblem4 teste = new TesteProblem4();
+        teste.testFindeBilligsteTastaturCorect();
+        teste.testFindeBilligsteTastaturFalsCorect();
+        teste.testFindeTeuerstenGegenstandCorect();
+        teste.testFindeTeuerstenGegenstandFalsCorect();
+        teste.testFindeTeuerstesUsbLaufwerkCorect();
+        teste.testFindeTeuerstesUsbLaufwerkFalsCorect();
+        teste.testBerechneAusgabenCorect();
+        teste.testBerechneAusgabenFalsCorect();
+    }
 }
